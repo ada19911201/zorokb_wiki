b587ede805dc2d4cfbb182cbb1568f7c61f148a9
zoro199112 <17702570531@163.com>
2023-03-24 19:55:44 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -19,7 +19,7 @@ extensions = ['recommonmark']
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'en'
+language = 'zh-CN'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
